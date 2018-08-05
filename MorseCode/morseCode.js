b705d681e6fd50823bdd617fef5291aead38924c
elizaRjb <elizarajbhandari@lftechnology.com>
2018-08-05 20:02:18 +0545

    Add MorseCode folder with morse code encoding function

---
+++ MorseCode/morseCode.js
@@ -0,0 +1,57 @@
+const morseCodeMap = {
+  'A' : '. -',
+  'B' : '- . . .',
+  'C' : '- . - .',
+  'D' : '- . .',
+  'E' : '.',
+  'F' : '. . - .',
+  'G' : '- - .',
+  'H' : '. . . .',
+  'I' : '. .',
+  'J' : '. - - -',
+  'K' : '- . -',
+  'L' : '. - . .',
+  'M' : '- -',
+  'N' : '- .',
+  'O' : '- - -',
+  'P' : '. - - .',
+  'Q' : '- - . -',
+  'R' : '. - .',
+  'S' : '. . .',
+  'T' : '-',
+  'U' : '. . -',
+  'V' : '. . . -',
+  'W' : '. - -',
+  'X' : '- . . -',
+  'Y' : '- . - -',
+  'Z' : '- - . .',
+  '1' : '. - - - -',
+  '2' : '. . - - -',
+  '3' : '. . . - -',
+  '4' : '. . . . -',
+  '5' : '. . . . .',
+  '6' : '- . . . .',
+  '7' : '- - . . .',
+  '8' : '- - - . .',
+  '9' : '- - - - .',
+  '0' : '- - - - -'
+};
+
+const spaceBetweenLetters = '   ';
+const spaceBetweenWords = '       ';
+
+function morseCodeEncoder(str){
+  var encodedMsg = '';
+  for(var i = 0; i < str.length; i++){
+    if(str.charAt(i) === ' '){
+      encodedMsg += spaceBetweenWords;
+    }
+    else{
+      encodedMsg += morseCodeMap[str.charAt(i).toUpperCase()] + spaceBetweenLetters;
+    }
+  }
+
+  console.log(encodedMsg);
+}
+
+morseCodeEncoder('a ba');
